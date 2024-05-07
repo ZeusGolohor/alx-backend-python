44284591dc6808c21b312886bbf762a4b7d5feec
zeusGolohor <zeusgolohor@gmail.com>
2024-05-06 18:07:59 -0700

    Import wait_random from the previous python file that you’ve written and write an async routine called wait_n that takes in 2 int arguments (in this order): n and max_delay. You will spawn wait_random n times with the specified max_delay.
    wait_n should return the list of all the delays (float values). The list of the delays should be in ascending order without using sort() because of concurrency.
    
    bob@dylan:~$ cat 1-main.py
    '''
    Test file for printing the correct output of the wait_n coroutine
    '''
    import asyncio
    
    wait_n = __import__('1-concurrent_coroutines').wait_n
    
    print(asyncio.run(wait_n(5, 5)))
    print(asyncio.run(wait_n(10, 7)))
    print(asyncio.run(wait_n(10, 0)))
    
    bob@dylan:~$ ./1-main.py
    [0.9693881173832269, 1.0264573845731002, 1.7992690129519855, 3.641373003434587, 4.500011569340617]
    [0.07256214141415429, 1.518551245602588, 3.355762808432721, 3.7032593997182923, 3.7796178143655546, 4.744537840582318, 5.50781365463315, 5.758942587637626, 6.109707751654879, 6.831351588271327]
    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
    The output for your answers might look a little different and that’s okay.

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,7 +2,14 @@
 '''
 Test file for printing the correct output of the wait_n coroutine
 '''
+import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int = 
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    A function to spawn wait_random n times with the specified max_delay.
+    """
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    return await asyncio.gather(*tasks)
