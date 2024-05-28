4adb5cb6f44cccfccb16c0e7a1621a6f1ac17973
zeusgolohor <zeusgolohor@gmail.com>
2024-05-28 01:55:26 +0000

    Read about memoization and familiarize yourself with the utils.memoize decorator.
    Implement the TestMemoize(unittest.TestCase) class with a test_memoize method.
    
    Inside test_memoize, define following class
    
    class TestClass:
    
        def a_method(self):
            return 42
    
        @memoize
        def a_property(self):
            return self.a_method()
    Use unittest.mock.patch to mock a_method. Test that when calling a_property twice, the correct result is returned but a_method is only called once using assert_called_once.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -59,3 +59,25 @@ class TestGetJson(unittest.TestCase):
         with patch("requests.get", return_value=Mock(**attributes)) as request:
             self.assertEqual(get_json(test_url), test_payload)
             request.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    A TestMemoize(unittest.TestCase) class with a test_memoize method.
+    """
+    def test_memoize(self):
+        """
+        A TestMemoize(unittest.TestCase) class with a test_memoize method.
+        """
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass, 'a_method', return_value=lambda: 42) as p:
+            test_ins = TestClass()
+            self.assertEqual(test_ins.a_property(), 42)
+            self.assertEqual(test_ins.a_property(), 42)
+            p.assert_called_once()
